8d7b572a83d8a28a9b68f264c3de39f4f80a3c69
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-06-28 12:08:27 -0400

    Checking lower and upper limit for the spawned processes

--- shell_script_v1.sh
+++ shell_script_v1.sh
@@ -4,12 +4,12 @@
 # based on the defined policies.
 # 
 # Policies:
-# 1. If a process exceeds the lower limit, set the flag to 1.
-#    If the flag is already set to 1, ignore the process. Wait 
-#    for X period of time and then email the admin, and set the
-#    flag to 0.
-# 2. If a process exceeds the upper limit, email the admin
-#    immediately and set the flag to 0.
+# [-] 1. If a process exceeds the lower limit, set the flag to 1.
+#     If the flag is already set to 1, ignore the process. Wait 
+#     for X period of time and then email the admin, and set the
+#     flag to 0.
+# [-] 2. If a process exceeds the upper limit, email the admin
+#     immediately and set the flag to 0.
 # 
 # Why not check the flag before checking the memory limit?
 # Currently, individual processes are checked for memory limit.
@@ -24,7 +24,8 @@
 # this process returns to normal before the X period of time,
 # operate as stated in policy 1.
 
-MEMORY_LIMIT=${1:-"82400"}
+LOWER_LIMIT=5000
+UPPER_LIMIT=3000000
 SLEEP_DURATION=${2:-"1"}
 
 function print_and_log() {
@@ -44,8 +45,11 @@ while true; do
 		drs=$(echo "$line" | awk '{print $2}')
 		time=$(date +%H:%M:%S)
 
-		if [[ $drs -gt $MEMORY_LIMIT ]]; then
+		if [[ $drs -gt $LOWER_LIMIT ]]; then
             exceeded_processes="$exceeded_processes\nPID: $pid, DRS: $drs"
+            if [[ $drs -gt $UPPER_LIMIT ]]; then
+                exceeded_processes="$exceeded_processes [exceeded upper limit]"
+            fi
 		fi
 	done <<< $ps_output
 
